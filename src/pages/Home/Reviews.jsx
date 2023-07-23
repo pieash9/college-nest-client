1a74a4f47f884df5dba2078153b1f89b5511ecaf
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-07-23 22:34:24 +0600

    added reviews section

--- src/pages/Home/Reviews.jsx
+++ src/pages/Home/Reviews.jsx
@@ -1,22 +1,29 @@
 import useFeedbackData from "../../hooks/useFeedbackData";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
 import "./styles/Reviews.css";
-
 // import required modules
 import { Pagination } from "swiper/modules";
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+import { FaRegStar, FaStar } from "react-icons/fa";
+import SectionTitle from "../../components/Title/SectionTitle";
 
 const Reviews = () => {
   const { feedbacks } = useFeedbackData();
   return (
     <>
+      <SectionTitle
+        Ftitle={"Reviews"}
+        Ltitle={"Experiences Shared"}
+        subTitle={"Read what others have to say about their experiences"}
+      />
       <Swiper
-        slidesPerView={4}
+        slidesPerView={2}
         spaceBetween={30}
         centeredSlides={true}
         pagination={{
@@ -25,15 +32,46 @@ const Reviews = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {feedbacks.length > 0 &&
+          feedbacks.slice(0, 9).map((feedback) => (
+            <div key={feedback._id}>
+              <SwiperSlide>
+                <div className=" text-start grid grid-cols-[1fr,6fr] h-64  gap-4 p-5">
+                  <div className="w-20 h-20 flex justify-center items-center">
+                    <img
+                      className="rounded-full w-40 h-40"
+                      src={feedback.candidateImage}
+                      alt=""
+                    />
+                  </div>
+                  <div className="w-full space-y-4">
+                    <Rating
+                      style={{ maxWidth: 100, color: "yellow" }}
+                      value={feedback.rating}
+                      readOnly
+                    />
+                    <p className="text-gray-600 text-sm text-justify">
+                      {feedback.feedback}
+                    </p>
+                    <div className="">
+                      <p className="text-gray-600 text-sm text-justify">
+                        <span className="text-gray-700 font-medium">
+                          {feedback.candidateName}{" "}
+                        </span>{" "}
+                        - Verified user
+                      </p>
+                      <p className="text-gray-600 text-sm text-justify">
+                        <span className="text-gray-700 font-medium">
+                          College name:{" "}
+                        </span>
+                        {feedback.collegeName}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </div>
+          ))}
       </Swiper>
     </>
   );
